f6117107624c714ee3b9354dd6d9821b4d4deaf0
Adam Stokes <adam.stokes@ubuntu.com>
2014-02-17 12:35:13 -0500

    prep for release
    Signed-off-by: Adam Stokes <adam.stokes@ubuntu.com>

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -4,18 +4,38 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         browserify: {
-            js: {
+            dist: {
                 files: {
-                    '../share/public/javascripts/app.js': ['js/app.js'],
-                    '../share/public/javascripts/admin.js': ['js/admin.js']
+                    '../build/js/app.js': [
+                        'bower_components/jquery/jquery.min.js',
+                        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                        'js/app.js'
+                    ]
+                },
+                beforeHook: function(bundle) {
+                    shim(bundle, {
+                        jquery: {
+                            path: 'bower_components/jquery/jquery.min.js',
+                            exports: '$'
+                        },
+                        bootstrap: {
+                            path: 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                            exports: null,
+                            depends: {
+                                jquery: '$'
+                            }
+                        }
+
+                    });
+
                 }
             }
         },
+
         uglify: {
             build: {
                 files: {
-                    '../share/public/javascripts/app-min.js': ['../public/javascripts/app.js'],
-                    '../share/public/javascripts/admin-min.js': ['../public/javascripts/admin.js']
+                    '../share/public/javascripts/app.min.js': ['../build/js/app.js']
                 }
             }
         },
@@ -26,19 +46,37 @@ module.exports = function(grunt) {
                     compress: true
                 },
                 files: {
-                    '../share/public/stylesheets/bundle.css': 'css/style.styl',
-                    '../share/public/stylesheets/admin.css': 'css/admin.styl'
+                    '../build/css/style.css': 'css/style.styl'
                 }
             }
-        }
+        },
+      concat: {
+          css: {
+              src: ['bower_components/bootswatch/cosmo/bootstrap.min.css',
+                  '../build/css/style.css'
+              ],
+              dest: '../build/css/combined.css'
+          }
+      },
+      cssmin: {
+          css: {
+              src: '../build/css/combined.css',
+              dest: '../share/public/stylesheets/bundle.css'
+          }
+      }
+
     });
 
     // Default task(s).
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-stylus');
     grunt.registerTask('default', ['browserify',
         'stylus',
+        'concat',
+        'cssmin',
         'uglify'
     ]);
 
